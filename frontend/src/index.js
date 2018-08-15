56658f1900bdbd6f6f527d471ef70c4b437589fd
Martin Spencer <m.spencer@1xinternet.de>
2018-08-15 12:48:11 +0200

    step 4, order js files into seperate dirs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import App from "./components/App";
+import registerServiceWorker from "./utils/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
