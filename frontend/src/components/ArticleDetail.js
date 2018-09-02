aadf390a591190c4bcc042c5e0b8e8782b23b3e1
Martin Spencer <m.spencer@1xinternet.de>
2018-09-02 12:39:27 +0200

    step 11, query for the article node in ArticleDetail component using the node ID

--- frontend/src/components/ArticleDetail.js
+++ frontend/src/components/ArticleDetail.js
@@ -1,5 +1,29 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 import PropTypes from "prop-types";
+
+const GET_ARTICLE = gql`
+  # Wrap the actual query so we can call it with a variable from the props.
+  query articleById($id: String!) {
+    nodeById(id: $id) {
+      ... on NodeArticle {
+        entityId
+        title
+        fieldImage {
+          alt
+          title
+          width
+          height
+          url
+        }
+        body {
+          processed
+        }
+      }
+    }
+  }
+`;
 
 const ArticleDetail = ({
   match: {
@@ -7,7 +31,19 @@ const ArticleDetail = ({
   }
 }) => {
   return (
-    <div className="ArticleDetail">I am an ArticleDetail! My ID is {id}</div>
+    <div className="ArticleDetail">
+      {/* variables are exposed to the graphql query prefixed with a $ sign. */}
+      <Query query={GET_ARTICLE} variables={{ id }}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+          // The actual entities are in data.nodeById
+          const { nodeById } = data;
+          console.log(nodeById);
+          return <div>I am an ArticleDetail! My ID is {id}</div>;
+        }}
+      </Query>
+    </div>
   );
 };
 
