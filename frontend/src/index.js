cfad338131f3f39598758004e8faf4503f7b7b19
Martin Spencer <m.spencer@1xinternet.de>
2018-09-04 10:27:33 +0200

    fixed wrong imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import "./components/index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import App from "./components/App";
+import "./index.css";
+import registerServiceWorker from "./utils/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
