a4bd504cd75b20f5901b338ea68762ea88b3cc0d
Martin Spencer <m.spencer@1xinternet.de>
2018-09-04 10:35:39 +0200

    using className instead of wrong class prop

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Loading = () => {
-  return <div class="Loading" aria-label="Loading content ..." />;
+  return <div className="Loading" aria-label="Loading content ..." />;
 };
 
 export default Loading;
